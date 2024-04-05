3b2ffd75e0faa1e61718861c360a45f8892801bd
Frantz Darbon <frantz.darbon@capgemini.com>
2024-04-05 15:57:54 +0200

    Refactoring repository in two separate modules

---
+++ events-service/src/main/java/com/capgemini/springboot/events/OrganizerRepository.java
@@ -0,0 +1,6 @@
+package com.capgemini.springboot.events;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrganizerRepository extends JpaRepository<Organizer, Integer> {
+}
