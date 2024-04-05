00c955193de11bfbcadd0cc1a378f0a0c0cb4901
Frantz Darbon <frantz.darbon@capgemini.com>
2024-04-05 15:40:21 +0200

    Ajout de la configuration externalisée

--- src/main/java/com/capgemini/springboot/tickets/events/EventRepository.java
+++ src/main/java/com/capgemini/springboot/tickets/events/EventRepository.java
@@ -3,7 +3,6 @@ package com.capgemini.springboot.tickets.events;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface EventRepository extends JpaRepository<Event, Integer> {
 
